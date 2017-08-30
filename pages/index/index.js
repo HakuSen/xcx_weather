4e1ddfa3e47cda884c32b24af7c5e81a4162b999
hexinxin <1427725103@qq.com>
2017-08-30 18:42:08 +0800

    hxx:finish

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,8 @@ Page({
   data: {
     basicdata:{},
     future:{},
-    city:''
+    city:'',
+    weatherstate:''
   },
   //get basci data
   basicData:function(){
@@ -49,15 +50,75 @@ Page({
           },
           success: function (res) {
             console.log(res.data)
+            var weekWeather = [];
+            var data = res.data.result.future
+            for (var i in data){
+              var str = data[i];
+              weekWeather.push(str)
+            }
+            console.log(weekWeather);
+            var weatherstate = that.skyVal(res.data.result.today.weather)[1];  //图标转换
             that.setData({
               basicdata: res.data.result,
-              future: res.data.result.future
+              future: weekWeather,
+              weatherstate: weatherstate
             })
           }
         })
 
       }
     })
+  },
+  //判断Skycons取值
+  skyVal: function (e) {
+    let x, y;
+    switch (e) {
+      case '晴':
+        x = '晴';
+        y = 'icon-weather';
+        break;
+      case '阴':
+        x = '阴';
+        y = 'icon-yin';
+        break;
+      case '小雨':
+        x = '小雨';
+        y = 'icon-weatherrainy';
+        break;
+      case '小雨转阴':
+        x = '小雨转阴';
+        y = 'icon-weathercloudy';
+        break;
+      case '中雨转小雨':
+        x = '中雨转小雨';
+        y = 'icon-yin';
+        break;
+      case '中雨':
+        x = '中雨';
+        y = 'icon-weatherrainy';
+        break;
+      case '小雨转多云':
+        x = '小雨转多云';
+        y = 'icon-tianqizitiku33';
+        break;
+      case '多云转晴':
+        x = '多云转晴';
+        y = 'icon-weather';
+        break;
+      case '风':
+        x = '风';
+        y = 'icon-weatherwindy';
+        break;
+      case '雾':
+        x = '雾';
+        y = 'icon-weatherfog';
+        break;
+      case '霾':
+        x = '霾';
+        y = 'icon-maihaze';
+        break;
+    }
+    return [x, y];
   },
   onLoad: function () {
     this.basicData()
