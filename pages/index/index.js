25df0981f49a5ed32a222bffab34c2c5f16fcdd9
hexinxin <1427725103@qq.com>
2017-08-30 14:44:27 +0800

    hxx:request+location

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,63 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    basicdata:{},
+    future:{},
+    city:''
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  //get basci data
+  basicData:function(){
+    let that = this;
+    //小程序获取坐标
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        console.log(latitude + ',' + longitude)
+        that.loadCity(longitude, latitude)
+      }
+    })
+  },
+  //百度地图api获取city名称
+  loadCity: function (longitude, latitude) {
+    var that = this
+    wx.request({
+      url: 'https://api.map.baidu.com/geocoder/v2/?ak=m5VfddjUAHr0U80TwMz5GBt27co0BEmY&location=' + latitude + ',' + longitude + '&output=json',
+      data: {},
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        // success  
+        console.log(res);
+        that.setData({ 
+          city: res.data.result.addressComponent.city
+        });
+
+        //小程序获取接口数据
+        wx.request({
+          url: 'http://v.juhe.cn/weather/index',
+          data: {
+            cityname: res.data.result.addressComponent.city,
+            key: '4875a2834124a91a2acda3b5b1808a88'
+          },
+          header: {
+            'content-type': 'application/json'
+          },
+          success: function (res) {
+            console.log(res.data)
+            that.setData({
+              basicdata: res.data.result,
+              future: res.data.result.future
+            })
+          }
+        })
+
+      }
     })
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+    this.basicData()
   }
 })
